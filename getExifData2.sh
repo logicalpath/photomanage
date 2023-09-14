4cc07578f0c2bf595bbfdc3f0fc090d33415e424
Eddie Dickey <eddie@logicalpath.com>
2023-09-14 09:34:55 -0700

    get exif data for files under fcpbundle; correct shellcheck errors

--- getExifData2.sh
+++ getExifData2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Ensure the first argument is 'folder' or 'photolib'
-if [ "$1" != "folder" ] && [ "$1" != "photolib" ]; then
-    echo "Error: First argument should be either 'folder' or 'photolib'"
+# Ensure the first argument is 'folder' or 'photolib' or fcpbundle
+if [ "$1" != "folder" ] && [ "$1" != "photolib" ] && [ "$1" != "fcpbundle" ]; then
+    echo "Error: First argument should be either 'folder' or 'photolib' or 'fcpbundle'"
     exit 1
 fi
 
@@ -29,6 +29,11 @@ echo "exif_file = $exif_file"
 # Decide which exiftool command to run based on the first argument
 if [ "$1" == "folder" ]; then
     exiftool -csv -"*GPS*" -"Date*" -r --ext json "$TOP_DIR" > "$exif_file"
-else
+elif [ "$1" == "photolib" ]; then
     find "$TOP_DIR" -type d -name 'originals' -exec exiftool -csv -"*GPS*" -"Date*" -r --ext ^json {} + > "$exif_file"
+else
+    # find "$TOP_DIR" -type d -name '.fcpbundle' -exec exiftool -csv -"*GPS*" -"Date*" -r --ext ^json {} + > "$exif_file"
+    find "$TOP_DIR" -type d -name '*.fcpbundle' -exec exiftool -csv -"*GPS*" -"Date*" -r --ext ^json {} + > "$exif_file"
+
+    
 fi
