5ce2921b544f8cebdfa36b2ddbfc2202a5b0351e
Eddie Dickey <eddie@logicalpath.com>
2023-10-01 11:57:27 -0700

    moving data to folders; exlcuding data from source control

--- scripts/getExifData.sh
+++ scripts/getExifData.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# Ensure the first argument is 'folder' or 'photolib'
-if [ "$1" != "folder" ] && [ "$1" != "photolib" ]; then
-    echo "Error: First argument should be either 'folder' or 'photolib'"
+# Ensure the first argument is 'folder' or 'photolib' or fcpbundle
+if [ "$1" != "folder" ] && [ "$1" != "photolib" ] && [ "$1" != "fcpbundle" ]; then
+    echo "Error: First argument should be either 'folder' or 'photolib' or 'fcpbundle'"
+    echo "Usage: ./getExifData2.sh folder ~/Feb2020/Movie-files"
+    echo "Usage: ./getExifData2.sh folder ~/Feb2020/GPhotos
+    echo "Usage: ./getExifData2.sh photolib ~/Photos"
+    echo "Usage: ./getExifData2.sh fcpbundle ~/Feb2020/Movies"
+    echo " Example to redirect stdout/stederr:
+    echo " ./getExifData2.sh folder ~/Feb2020/GPhotos > GPhotos.log 2>&1"
     exit 1
 fi
+
 
 # Path to the top-level directory
 TOP_DIR=$2
@@ -13,8 +20,8 @@ echo "Searching $TOP_DIR for media files..."
 folder=$(./get-basename.sh "$2")
 echo "The extracted folder name is: $folder"
 
-exif_file="$folder-exif.csv"
-
+# exif_file="$folder-exif.csv"
+exif_file="../exifs/$folder-exif.csv"
 
 echo "Exif file: $exif_file"
 
@@ -28,7 +35,14 @@ echo "exif_file = $exif_file"
 
 # Decide which exiftool command to run based on the first argument
 if [ "$1" == "folder" ]; then
-    exiftool -csv -"*GPS*" -"Date*" -r --ext json "$TOP_DIR" > "$exif_file"
+    # exiftool -csv -"*GPS*" -"Create*Date" -r --ext json "$TOP_DIR" > "$exif_file"
+    exiftool -csv -"Create*Date" -n -gpslatitude -gpslongitude -r --ext json "$TOP_DIR" > "$exif_file"
+
+elif [ "$1" == "photolib" ]; then
+    find "$TOP_DIR" -type d -name 'originals' -exec exiftool -csv -"Create*Date" -n -gpslatitude -gpslongitude -r --ext json {} + > "$exif_file"
 else
-    find "$TOP_DIR" -type d -name 'originals' -exec exiftool -csv -"*GPS*" -"Date*" -r --ext ^json {} + > "$exif_file"
+    # find "$TOP_DIR" -type d -name '.fcpbundle' -exec exiftool -csv -"*GPS*" -"Date*" -r --ext ^json {} + > "$exif_file"
+    # find "$TOP_DIR" -type d -name '*.fcpbundle' -exec exiftool -csv -"*GPS*" -"Create*Date" -r --ext ^plist --ext ^json {} + > "$exif_file"
+    find "$TOP_DIR" -type d -name '*.fcpbundle' -exec exiftool -csv -"Create*Date" -n -gpslatitude -gpslongitude -r --ext plist {} + > "$exif_file"
+
 fi
